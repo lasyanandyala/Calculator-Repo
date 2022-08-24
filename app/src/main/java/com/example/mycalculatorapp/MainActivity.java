ddd98435114afed73eb722a6b1e20e79b286e246
lasyanandyala <90787203+lasyanandyala@users.noreply.github.com>
2022-08-24 08:19:52 -0500

    AddedNewEnhancements

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -20,11 +20,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int sum = num1 + num2;
-
-        numberSumTV.setText("" + sum);
+        try {
+            if((number1ET.getText().toString()).equals("") || (number2ET.getText().toString()).equals("")){
+                numberSumTV.setText("Try Again");
+            }
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int sum = num1+num2;
+            numberSumTV.setText("Answer: " + sum);
+        }catch(Exception e){
+            numberSumTV.setText("Try Again");
+        }
     }
 
     public void findDiff(View view) {
@@ -32,11 +38,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int diff = num1 - num2;
-
-        numberSumTV.setText("" + diff);
+        try {
+            if((number1ET.getText().toString()).equals("") || (number2ET.getText().toString()).equals("")){
+                numberSumTV.setText("Try Again");
+            }
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int diff = num1-num2;
+            numberSumTV.setText("Answer: " + diff);
+        }catch(Exception e){
+            numberSumTV.setText("Try Again");
+        }
     }
 
     public void findDiv(View view) {
@@ -44,24 +56,35 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int diff = num1 / num2;
-        double extra = num1 % num2;
-
-        numberSumTV.setText("" + (double)diff + extra);
+        try {
+            if((number1ET.getText().toString()).equals("") || (number2ET.getText().toString()).equals("")){
+                numberSumTV.setText("Try Again");
+            }
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int div = num1 / num2;
+            double extra = num1 % num2;
+            numberSumTV.setText("Answer: " + div + extra);
+        }catch(Exception e){
+            numberSumTV.setText("Try Again");
+        }
     }
 
     public void findMul(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
-
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        double mul = (double)num1 * num2;
-
-        numberSumTV.setText("" + mul);
+        try {
+            if((number1ET.getText().toString()).equals("") || (number2ET.getText().toString()).equals("")){
+                numberSumTV.setText("Try Again");
+            }
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int mul = num1 * num2;
+            numberSumTV.setText("Answer: " + mul);
+        }catch(Exception e){
+            numberSumTV.setText("Try Again");
+        }
     }
 
     public void findMod(View view) {
@@ -69,11 +92,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int mod = num1%num2;
-
-        numberSumTV.setText("" + mod);
+        try {
+            if((number1ET.getText().toString()).equals("") || (number2ET.getText().toString()).equals("")){
+                numberSumTV.setText("Try Again");
+            }
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int mod = num1 % num2;
+            numberSumTV.setText("Answer: " + mod);
+        }catch(Exception e){
+            numberSumTV.setText("Try Again");
+        }
     }
 
 }
